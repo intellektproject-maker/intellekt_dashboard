03af6c61a80d7316c6816e313148def6c08197cd
Gauthami <gauthami123890@gmail.com>
2026-04-30 11:46:38 +0530

    Fix SSL for Railway DB

--- intellekt-dashboard/backend/db.js
+++ intellekt-dashboard/backend/db.js
@@ -4,10 +4,11 @@ const path = require("path");
 require("dotenv").config({
   path: path.join(__dirname, ".env"),
 });
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
